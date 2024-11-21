1c40d70b817b1688c686ee20420241a1fcdc3d8d
Chenfeng-dev <chenfengdev@outlook.com>
2024-11-21 19:28:00 +0530

    chenfeng: Use Soong namespace to avoid device build conflicts

--- lineage_chenfeng.mk
+++ lineage_chenfeng.mk
@@ -18,16 +18,16 @@ PRODUCT_NAME := lineage_chenfeng
 PRODUCT_DEVICE := chenfeng
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := 24069PC21I
+PRODUCT_MODEL := 24053PY09G
 
-PRODUCT_SYSTEM_NAME := chenfeng_in
+PRODUCT_SYSTEM_NAME := chenfeng_global
 PRODUCT_SYSTEM_DEVICE := chenfeng
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="missi_phone_global_only64-user 14 UKQ1.240116.001 V816.0.6.0.UNJINXM release-keys" \
+    PRIVATE_BUILD_DESC="chenfeng_global-user 14 UKQ1.240116.001 V816.0.6.0.UNJINXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Android/missi_phone_global_only64/missi:14/UKQ1.240116.001/V816.0.6.0.UNJINXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/chenfeng_global/chenfeng:14/UKQ1.240116.001/V816.0.6.0.UNJINXM:user/release-keys
